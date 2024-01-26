ac6591a96aecbcd52e676ef4e7a3e6cd2feb83fb
leocarten <leothelinuxguy@outlook.com>
2024-01-26 16:42:54 -0500

    Correct items query v23

--- endpoints/showUsersInFeed.js
+++ endpoints/showUsersInFeed.js
@@ -2,8 +2,8 @@ import pool from '../db/connectionPool.js';
 
 export const showItemsInFeed = (req) => {
     return new Promise((resolve, reject) => {
-        // first_name,dob,bio,bucket_list,interests,pet_preference,bitmoji_type,pictures,job,music_preference
-        pool.query('SELECT * FROM info_to_display', (err, result, fields) => {
+        // eventually I need to add parameters here for picking certain genders, app purposes, etc.
+        pool.query('SELECT first_name,dob,bio,bucket_list,interests,pet_preference,bitmoji_type,pictures,is_verified,job,music_preference FROM info_to_display', (err, result, fields) => {
             if (err) {
               reject(err);
             } else {
