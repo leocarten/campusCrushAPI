69c7f5dd941488086a1942e63cb2f1707ad1085f
leocarten <leothelinuxguy@outlook.com>
2024-01-26 16:38:31 -0500

    Correct items query v2

--- endpoints/showUsersInFeed.js
+++ endpoints/showUsersInFeed.js
@@ -2,7 +2,8 @@ import pool from '../db/connectionPool.js';
 
 export const showItemsInFeed = (req) => {
     return new Promise((resolve, reject) => {
-        pool.query('SELECT first_name,dob,bio,bucket_list,interests,pet_preference,bitmoji_type,pictures,job,music_preference FROM info_to_display', (err, result, fields) => {
+        // first_name,dob,bio,bucket_list,interests,pet_preference,bitmoji_type,pictures,job,music_preference
+        pool.query('SELECT * FROM info_to_display', (err, result, fields) => {
             if (err) {
               reject(err);
             } else {
