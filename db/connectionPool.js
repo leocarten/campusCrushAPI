9d493bf9630e7516356005fa576f4c6276d87a24
leocarten <leothelinuxguy@outlook.com>
2024-01-26 09:48:26 -0500

    Whoops

--- db/connectionPool.js
+++ db/connectionPool.js
@@ -2,11 +2,6 @@ import { createPool } from 'mysql';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.HOST_id);
-console.log(process.env.USER_id);
-console.log(process.env.PASSWORD_id);
-console.log(process.env.DATABASE_id);
-
 const pool = createPool({
   host: process.env.HOST_id,
   user: process.env.USER_id,
