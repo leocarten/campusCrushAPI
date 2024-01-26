308d427b2ff5e89dfac596f0159f756bc63bd9ef
EC2 Default User <ec2-user@ip-172-31-27-254.us-east-2.compute.internal>
2024-01-26 14:58:09 +0000

    Debugging

--- endpoints/authenticateUser.js
+++ endpoints/authenticateUser.js
@@ -1,11 +1,11 @@
-import { verifyToken } from "../jwt/verifyJwt"
+import { authenticateUsersJWT } from "../jwt/verifyJwt"
 import dotenv from 'dotenv';
 dotenv.config();
 
 export const authenticateUser = (req) => {
     return new Promise((resolve, reject) => {
         const tokenFromUser = req.body;
-        if(verifyToken(tokenFromUser, process.env.ACCESS_SECRET_KEY) != false){
+        if(authenticateUsersJWT(tokenFromUser, process.env.ACCESS_SECRET_KEY) != false){
             resolve({success: true, message: "User has been authenticated!"})
         }else{
             reject({success: false, message: "Invalid access token."})
--- jwt/verifyJwt.js
+++ jwt/verifyJwt.js
@@ -1,9 +1,14 @@
-import jwt from 'jsonwebtoken';
-export const verifyToken = (token,key) => {
-    try{
-        const clientToken = jwt.verify(token, key);
-        return clientToken;
-    }catch(err){
-        return false;
-    }
+import { verifyToken } from "../jwt/verifyJwt"
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const authenticateUsersJWT = (req) => {
+    return new Promise((resolve, reject) => {
+        const tokenFromUser = req.body;
+        if(verifyToken(tokenFromUser, process.env.ACCESS_SECRET_KEY) != false){
+            resolve({success: true, message: "User has been authenticated!"})
+        }else{
+            reject({success: false, message: "Invalid access token."})
+        }
+    })
 }
