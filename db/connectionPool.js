f7d5698de5cb97ac87cb9176c82b9c212d0873d6
EC2 Default User <ec2-user@ip-172-31-27-254.us-east-2.compute.internal>
2024-01-26 03:18:50 +0000

    Working connection

--- db/connectionPool.js
+++ db/connectionPool.js
@@ -2,11 +2,16 @@ import { createPool } from 'mysql';
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log(process.env.HOST_id);
+console.log(process.env.USER_id);
+console.log(process.env.PASSWORD_id);
+console.log(process.env.DATABASE_id);
+
 const pool = createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  host: process.env.HOST_id,
+  user: process.env.USER_id,
+  password: process.env.PASSWORD_id,
+  database: process.env.DATABASE_id
 });
 
 export default pool;
