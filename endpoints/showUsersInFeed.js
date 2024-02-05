be04853d62ec2424b7eb1e33d2a0672af3b1e3d0
leocarten <leothelinuxguy@outlook.com>
2024-02-05 10:13:45 -0500

    Testing user id

--- endpoints/showUsersInFeed.js
+++ endpoints/showUsersInFeed.js
@@ -7,11 +7,9 @@ export const showItemsInFeed = (token) => {
         // eventually I need to add parameters here for picking certain genders, app purposes, etc.
 
         // lets get the users id
-
-        console.log(token);
-
         const decodedToken = jwtDecode(token);
-        console.log(decodedToken);
+        const id = decodedToken['id'];
+        console.log(id);
 
         pool.query('SELECT first_name,dob,bio,bucket_list,interests,pet_preference,app_purpose,bitmoji_type,pictures,is_verified,job,music_preference,has_tattoos,sleep_schedule,win_my_heart,workout FROM info_to_display', (err, result, fields) => {
             if (err) {
