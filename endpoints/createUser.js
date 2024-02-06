0593f4e16cde69913e0a427c8073348e7bd50e5d
leocarten <leothelinuxguy@outlook.com>
2024-02-06 09:41:06 -0500

    Updated JWT including the gender user wants to see v8

--- endpoints/createUser.js
+++ endpoints/createUser.js
@@ -78,6 +78,7 @@ export const createUser = (req) => {
                         if (results && results.length > 0 && results[0].max_id !== null) {
                             const user_id = results[0].max_id;
                             const genderUserWantsToBeShown = result[0].genderUserWantsToSee;
+                            console.log("wants to be shown:",genderUserWantsToBeShown);
                             const new_query = 'INSERT INTO info_to_display (id, first_name, dob, bio, gender, bucket_list, interests, pet_preference, app_purpose, bitmoji_type, pictures, is_verified, elo_score, location, has_top_placement, job, music_preference, has_tattoos, sleep_schedule, win_my_heart, workout, genderUserWantsToSee) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
                             const bitmoji_type = 1;
                             const is_verified = 0;
