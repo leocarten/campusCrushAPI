110aac28228a49f440954cfbe2cd4721e179da2e
leocarten <leothelinuxguy@outlook.com>
2024-02-17 15:21:12 -0500

    Remove neural network

--- neuralNetwork/statistics/movieGenre.js
+++
@@ -1,8 +0,0 @@
-export const movieStats = (genre1, genre2) => {
-    if(genre1 == genre2){
-        return 1;
-    }
-    else{
-        return 0.35;
-    }
-}
--- neuralNetwork/statistics/petPreference.js
+++
@@ -1,8 +0,0 @@
-export const petStats = (user1, user2) => {
-    if(user1 == user2){
-        return 1;
-    }
-    else{
-        return 0.35;
-    }
-}
