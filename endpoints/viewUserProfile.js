167450ba35a3566e635391bb5b353bd24e2cfd6c
leocarten <leothelinuxguy@outlook.com>
2024-02-18 17:29:55 -0500

    Added user profile endpoint v5

--- endpoints/viewUserProfile.js
+++ endpoints/viewUserProfile.js
@@ -2,11 +2,11 @@ import pool from '../db/connectionPool.js';
 import { authenticateUsersJWT } from '../jwt/verifyJwt.js';
 import { jwtDecode } from "jwt-decode";
 
-export const viewUserProfile = (req) => {
+export const viewUserProfile = (token) => {
     console.log('req from view endpoint:',req.body);
   return new Promise((resolve, reject) => {
     const token = req.body;
-    if(verifyToken(token, process.env.ACCESS_SECRET_KEY) != false){
+    // if(verifyToken(token, process.env.ACCESS_SECRET_KEY) != false){
       const decodedToken = jwtDecode(token);
       const id = decodedToken['id'];
       pool.query('SELECT first_name,dob,bio,bucket_list,interests,pet_preference,app_purpose,bitmoji_type,pictures,is_verified,job,music_preference,has_tattoos,sleep_schedule,win_my_heart,workout,communication_style,ideal_first_meetup FROM info_to_display WHERE id = ?', [id], (err, result, fields) => {
@@ -16,6 +16,6 @@ export const viewUserProfile = (req) => {
           resolve(result);
         }
       });
-    }
+    // }
   });
 };
