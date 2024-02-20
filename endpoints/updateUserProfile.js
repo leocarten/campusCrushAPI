964be522c82e140bfab2fd700a6e83666d919753
leocarten <leothelinuxguy@outlook.com>
2024-02-20 08:30:22 -0500

    Testing json objects

--- endpoints/updateUserProfile.js
+++ endpoints/updateUserProfile.js
@@ -3,12 +3,13 @@ import { authenticateUsersJWT } from '../jwt/verifyJwt.js';
 import { jwtDecode } from "jwt-decode";
 
 export const updateUserProfile = (token, thingsToUpdate) => {
-
+  let updateQuery = "UPDATE info_to_display SET ";
+  let updates = [];
   const mapOfValues = {
     "bio":"",
     "bucket_list":"",
-    "interests_hobbies":"",
-    "music_preference":"",
+    "interests_hobbies":[],
+    "music_preference":[],
     "pet_preference":"",
     "app_purpose":"",
     "wants_to_be_shown":"",
@@ -21,13 +22,24 @@ export const updateUserProfile = (token, thingsToUpdate) => {
     "ideal_first_meetup":""
   }
 
+  for(const value in thingsToUpdate){
+    if(value != "type" && value != ""){
+        if(thingsToUpdate[value] != "" && thingsToUpdate[value] != []){
+            updates.push('${value} = ${thingsToUpdate[value]}');
+        }
+    }
+  }
+
   return new Promise((resolve, reject) => {
     // if(verifyToken(token, process.env.ACCESS_SECRET_KEY) != false){
       const decodedToken = jwtDecode(token);
       console.log('the decoded token:',decodedToken);
       const id = decodedToken['id'];
       console.log('id:',id);
-      console.log('\ndata from user:\n',thingsToUpdate);
+      if(updates.length > 0){
+        console.log('updates test:\n',updates);
+      }
+    //   console.log('\ndata from user:\n',thingsToUpdate);
       resolve("Done.");
     //   pool.query('UPDATE info_to_display  WHERE id = ?', [id], (err, result, fields) => {
     //     if (err) {
