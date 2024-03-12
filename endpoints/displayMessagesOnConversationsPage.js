d8721d60ac66d20273ae0276ff7b06c3ab950412
leocarten <leothelinuxguy@outlook.com>
2024-03-12 10:43:31 -0400

    Testing names v6

--- endpoints/displayMessagesOnConversationsPage.js
+++ endpoints/displayMessagesOnConversationsPage.js
@@ -13,16 +13,49 @@ export const displayConversations = (token, otherUserId) => {
         console.log('sender id:', requestID);
 
         // query to make sure it doesn't exist:
-        const getConversations = 'SELECT mostRecentMessage, IdOfPersonWhoSentLastMessage, hasOpenedMessage FROM all_messages_interface WHERE originalSenderID = ? OR originalRecieverID = ?';
+        const getConversations = 'SELECT mostRecentMessage, IdOfPersonWhoSentLastMessage, hasOpenedMessage, originalSenderID, originalRecieverID FROM all_messages_interface WHERE originalSenderID = ? OR originalRecieverID = ?';
         pool.query(getConversations, [requestID, requestID], (queryErr, resultsForConversation) => {
             if (queryErr) {
                 console.error('Error executing first query: ', queryErr);
                 reject(queryErr)
             } else if (resultsForConversation.length !== 0) { // it exists!
                 console.log("Response:",resultsForConversation);
-                const ids = resultsForConversation.map(result => result.IdOfPersonWhoSentLastMessage);
-                console.log(ids);
-                resolve({success: true, conversations: resultsForConversation});
+                const idsOfSenders = resultsForConversation.map(result => result.originalSenderID);
+                const idsOfRecievers = resultsForConversation.map(result => result.originalRecieverID);
+                var namesOfPersonHavingConvoWith = []
+                // var i = idsOfSenders.length;
+                for(var i = 0; i < idsOfSenders.length; i++){
+                    if (requestID == idsOfSenders[i]){
+                        const nameQuery = 'SELECT first_name from info_to_display where id = ?';
+                        pool.query(nameQuery, [requestID], (nameQueryError, nameOfUser) => {
+                            if(nameQueryError){
+                                console.error('Error executing second query: ', queryErr);
+                                reject(queryErr);
+                            }else if(nameOfUser.length == 1){
+                                namesOfPersonHavingConvoWith.append(nameOfUser);
+                            }else{
+                                resolve({ success: false, message: "Error" });
+                            }
+                        } )
+                    }
+                    else{
+                        const nameQuery = 'SELECT first_name from info_to_display where id = ?';
+                        pool.query(nameQuery, [idsOfRecievers[i]], (nameQueryError, nameOfUser) => {
+                            if(nameQueryError){
+                                console.error('Error executing second query: ', queryErr);
+                                reject(queryErr);
+                            }else if(nameOfUser.length == 1){
+                                namesOfPersonHavingConvoWith.append(nameOfUser);
+                            }else{
+                                resolve({ success: false, message: "Error" });
+                            }
+                        } )
+                    }
+                }
+
+                // console.log(ids);
+                resolve({success: true, conversations: resultsForConversation, names: namesOfPersonHavingConvoWith});
+
 
                 // const getNameOfOtherUser = 'SELECT first_name from first_name where id = ?'
                 // pool.query(getNameOfOtherUser, [])
