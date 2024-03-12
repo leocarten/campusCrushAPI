a3e4768530dadd7b17d01d1a771d6495937e6012
leocarten <leothelinuxguy@outlook.com>
2024-03-12 19:47:46 -0400

    SPELLING MISTAKE

--- endpoints/sendAdditionalMessages.js
+++ endpoints/sendAdditionalMessages.js
@@ -30,7 +30,7 @@ export const sendAdditionalMessages = (token, message, id1, id2) => {
         }
 
         // query to make sure it doesn't exist:
-        const getConvoID = 'SELECT convoID FROM all_messages_interface WHERE originalSenderID = ? AND originalRecieverID = ? or originalSenderID = ? AND originalRecieverI';
+        const getConvoID = 'SELECT convoID FROM all_messages_interface WHERE originalSenderID = ? AND originalRecieverID = ? or originalSenderID = ? AND originalRecieverID';
         pool.query(getConvoID, [trueSender, trueReciever, trueReciever, trueSender], (queryErr, queryCheckResults) => {
             if (queryErr) {
                 console.error('Error executing first query: ', queryErr);
