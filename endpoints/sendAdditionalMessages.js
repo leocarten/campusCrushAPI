788a9e9933b46fb6a543c0a08ae0ecb514fdc79c
leocarten <leothelinuxguy@outlook.com>
2024-03-27 18:04:06 -0400

    Testing messaging v3

--- endpoints/sendAdditionalMessages.js
+++ endpoints/sendAdditionalMessages.js
@@ -21,49 +21,79 @@ export const sendAdditionalMessages = (token, message, id1, id2) => {
         // const IdOfPersonWhoSentLastMessage = senderID;
         const hasOpenedMessage = 0;
 
-        if(id1 == senderID){
+        if (id1 == senderID) {
             trueSender = senderID;
             trueReciever = id2;
-        }else if(id2 == senderID){
+        } else if (id2 == senderID) {
             trueSender = id2;
             trueReciever = id1;
-            const getConvoID = 'SELECT convoID FROM all_messages_interface WHERE originalSenderID = ? AND originalRecieverID = ? or originalSenderID = ? AND originalRecieverID';
-            pool.query(getConvoID, [trueSender, trueReciever, trueSender, trueReciever], (queryErr, queryCheckResults) => {
-                if (queryErr) {
-                    console.error('Error executing first query: ', queryErr);
-                    reject(queryErr);
-                } else if (queryCheckResults.length !== 0) { // it exists!
-                    console.log("the results for the query:",queryCheckResults)
-                    const convoID = queryCheckResults[0].convoID;
-                    const insertIntoMessagesQuery = 'INSERT INTO messages (convoID, messageContent, senderID) VALUES (?, ?, ?)';
-                    console.log('true sender:',trueSender);
-                    const values = [convoID, message, trueSender];
-                    pool.query(insertIntoMessagesQuery, values, (queryErr, result) => {
-                        if (queryErr) {
-                            console.error('Error executing second query:', queryErr);
-                            server_error = true;
-                            reject(queryErr);
-                        } else {
-                            const updateLastSentMessage = 'UPDATE all_messages_interface SET mostRecentMessage = ?, IdOfPersonWhoSentLastMessage = ?, hasOpenedMessage = ? WHERE convoID = ?';
-                            pool.query(updateLastSentMessage, [message, senderID, hasOpenedMessage, convoID], (queryError, result) =>{
-                                if(queryError){
-                                    console.error('Error executing third query:', queryError);
-                                    server_error = true;
-                                    reject(queryError);
-                                }
-                                else{
-                                    resolve({success: true});
-                                }
-                            })
-                        }
-                    });
-                } else {
-                    
-                }
-            });
-        }else{
+        } else {
             resolve({ success: false, message: "Error with IDs." });
         }
-
-    });
-};
+        
+        const getConvoID = 'SELECT convoID FROM all_messages_interface WHERE originalSenderID = ? AND originalRecieverID = ?';
+        pool.query(getConvoID, [trueSender, trueReciever], (queryErr, queryCheckResults) => {
+            if (queryErr) {
+                console.error('Error executing first query: ', queryErr);
+                reject(queryErr);
+            } else if (queryCheckResults.length == 1) { // it exists for 1 conversation
+                console.log("the results for the query:", queryCheckResults)
+                const convoID = queryCheckResults[0].convoID;
+                const insertIntoMessagesQuery = 'INSERT INTO messages (convoID, messageContent, senderID) VALUES (?, ?, ?)';
+                console.log('true sender:', trueSender);
+                const values = [convoID, message, trueSender];
+                pool.query(insertIntoMessagesQuery, values, (queryErr, result) => {
+                    if (queryErr) {
+                        console.error('Error executing second query:', queryErr);
+                        server_error = true;
+                        reject(queryErr);
+                    } else {
+                        const updateLastSentMessage = 'UPDATE all_messages_interface SET mostRecentMessage = ?, IdOfPersonWhoSentLastMessage = ?, hasOpenedMessage = ? WHERE convoID = ?';
+                        pool.query(updateLastSentMessage, [message, senderID, hasOpenedMessage, convoID], (queryError, result) => {
+                            if (queryError) {
+                                console.error('Error executing third query:', queryError);
+                                server_error = true;
+                                reject(queryError);
+                            } else {
+                                resolve({ success: true });
+                            }
+                        })
+                    }
+                });
+            } else {
+                const getConvoIDv2 = 'SELECT convoID FROM all_messages_interface WHERE originalSenderID = ? AND originalRecieverID = ?';
+                pool.query(getConvoIDv2, [trueReciever, trueSender], (queryErr, queryCheckResults) => {
+                    if (queryErr) {
+                        console.error('Error executing first query: ', queryErr);
+                        reject(queryErr);
+                    } else if (queryCheckResults.length == 1) { // it exists for 1 conversation
+                        console.log("the results for the query:", queryCheckResults)
+                        const convoID = queryCheckResults[0].convoID;
+                        const insertIntoMessagesQuery = 'INSERT INTO messages (convoID, messageContent, senderID) VALUES (?, ?, ?)';
+                        console.log('true sender:', trueSender);
+                        const values = [convoID, message, trueSender];
+                        pool.query(insertIntoMessagesQuery, values, (queryErr, result) => {
+                            if (queryErr) {
+                                console.error('Error executing second query:', queryErr);
+                                server_error = true;
+                                reject(queryErr);
+                            } else {
+                                const updateLastSentMessage = 'UPDATE all_messages_interface SET mostRecentMessage = ?, IdOfPersonWhoSentLastMessage = ?, hasOpenedMessage = ? WHERE convoID = ?';
+                                pool.query(updateLastSentMessage, [message, senderID, hasOpenedMessage, convoID], (queryError, result) => {
+                                    if (queryError) {
+                                        console.error('Error executing third query:', queryError);
+                                        server_error = true;
+                                        reject(queryError);
+                                    } else {
+                                        resolve({ success: true });
+                                    }
+                                })
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    })
+}
+        
