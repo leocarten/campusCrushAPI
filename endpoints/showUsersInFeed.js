7289fdd8e8a1a54396f293d5430e78685c1c5a9c
leocarten <leothelinuxguy@outlook.com>
2024-03-31 20:46:44 -0400

    real network spelling mistake lol v4

--- endpoints/showUsersInFeed.js
+++ endpoints/showUsersInFeed.js
@@ -3,6 +3,7 @@ import { jwtDecode } from "jwt-decode";
 import trainedNet from '../neuralNetwork_v2/trained-net.js';
 
 function calculateCompatibility(row){
+    // this is where we need to get all the data and convert it into model parameters
     const modelResult = trainedNet([1,1,1,1,1,1,1,1,1,1])[0];
     return modelResult;
 }
@@ -22,55 +23,99 @@ export const showItemsInFeed = (token) => {
         console.log("proximity:",proximity);
         console.log(proximity*2);
 
-        // pool.query(`SELECT first_name,dob,bio,bucket_list,interests,pet_preference,app_purpose,bitmoji_type,pictures,is_verified,job,music_preference,has_tattoos,sleep_schedule,win_my_heart,workout,communication_style,ideal_first_meetup 
-        //               FROM info_to_display 
-        //               WHERE id != ? 
-        //               AND 
-        //               gender = ?`, [id, genderUserWantsToSee],(err, result, fields) => {
-        pool.query(`
-          SELECT 
-              info_to_display.id,first_name, dob, bio, bucket_list, interests, pet_preference, app_purpose, bitmoji_type, pictures, is_verified, job, music_preference, has_tattoos, sleep_schedule, win_my_heart, workout, communication_style, ideal_first_meetup, distance
-          FROM 
-              (
-                  SELECT 
-                      id,
-                      3958.8 * 2 * ASIN(
-                          SQRT(
-                              POWER(
-                                  SIN((lat2 - abs(lat1)) * pi()/180 / 2), 2
-                              ) + COS(lat2 * pi()/180 ) * COS(abs(lat1) * pi()/180) * POWER(SIN((long2 - long1) * pi()/180 / 2), 2)
-                          )
-                      ) AS distance
-                  FROM 
-                      (
-                          SELECT 
-                              id,
-                              ? AS lat2, 
-                              ? AS long2, 
-                              lat AS lat1, 
-                              long_ AS long1
-                          FROM 
-                              info_to_display
-                          WHERE
-                              id != ? and gender = ? and lat != 0.0 and long_ != 0.0
-                      ) AS p
-              ) AS distance_table
-          JOIN 
-              info_to_display ON info_to_display.id = distance_table.id
-          WHERE 
-          distance < ?`, [lat, long_, id, genderUserWantsToSee, proximity],(err, result, fields) => {
-            if (err) {
-                reject(err);
-            } else {
-                console.log("Result before adding compatibility:", result);
-                result.forEach(row => {
-                    // Calculate compatibility here based on the fields in the 'row'
-                    row.compatibility = calculateCompatibility(row);
-                    console.log("Compatibility calculated for row:", row.compatibility);
-                });
-                console.log("Result after adding compatibility:", result);
-                resolve(result);
-            }
-          });
+        if(genderUserWantsToSee == 1 || genderUserWantsToSee){
+            pool.query(`
+            SELECT 
+                info_to_display.id,first_name, dob, bio, bucket_list, interests, pet_preference, app_purpose, bitmoji_type, pictures, is_verified, job, music_preference, has_tattoos, sleep_schedule, win_my_heart, workout, communication_style, ideal_first_meetup, distance
+            FROM 
+                (
+                    SELECT 
+                        id,
+                        3958.8 * 2 * ASIN(
+                            SQRT(
+                                POWER(
+                                    SIN((lat2 - abs(lat1)) * pi()/180 / 2), 2
+                                ) + COS(lat2 * pi()/180 ) * COS(abs(lat1) * pi()/180) * POWER(SIN((long2 - long1) * pi()/180 / 2), 2)
+                            )
+                        ) AS distance
+                    FROM 
+                        (
+                            SELECT 
+                                id,
+                                ? AS lat2, 
+                                ? AS long2, 
+                                lat AS lat1, 
+                                long_ AS long1
+                            FROM 
+                                info_to_display
+                            WHERE
+                                id != ? and gender = ? and lat != 0.0 and long_ != 0.0
+                        ) AS p
+                ) AS distance_table
+            JOIN 
+                info_to_display ON info_to_display.id = distance_table.id
+            WHERE 
+            distance < ?`, [lat, long_, id, genderUserWantsToSee, proximity],(err, result, fields) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    // console.log("Result before adding compatibility:", result);
+                    result.forEach(row => {
+                        // Calculate compatibility here based on the fields in the 'row'
+                        row.compatibility = calculateCompatibility(row);
+                        console.log("Compatibility calculated for row:", row.compatibility);
+                    });
+                    // console.log("Result after adding compatibility:", result);
+                    resolve(result);
+                }
+            });
+        }
+        else{
+            pool.query(`
+            SELECT 
+                info_to_display.id,first_name, dob, bio, bucket_list, interests, pet_preference, app_purpose, bitmoji_type, pictures, is_verified, job, music_preference, has_tattoos, sleep_schedule, win_my_heart, workout, communication_style, ideal_first_meetup, distance
+            FROM 
+                (
+                    SELECT 
+                        id,
+                        3958.8 * 2 * ASIN(
+                            SQRT(
+                                POWER(
+                                    SIN((lat2 - abs(lat1)) * pi()/180 / 2), 2
+                                ) + COS(lat2 * pi()/180 ) * COS(abs(lat1) * pi()/180) * POWER(SIN((long2 - long1) * pi()/180 / 2), 2)
+                            )
+                        ) AS distance
+                    FROM 
+                        (
+                            SELECT 
+                                id,
+                                ? AS lat2, 
+                                ? AS long2, 
+                                lat AS lat1, 
+                                long_ AS long1
+                            FROM 
+                                info_to_display
+                            WHERE
+                                id != ? and gender = 1 or gender = 2 and lat != 0.0 and long_ != 0.0
+                        ) AS p
+                ) AS distance_table
+            JOIN 
+                info_to_display ON info_to_display.id = distance_table.id
+            WHERE 
+            distance < ?`, [lat, long_, id, proximity],(err, result, fields) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    // console.log("Result before adding compatibility:", result);
+                    result.forEach(row => {
+                        // Calculate compatibility here based on the fields in the 'row'
+                        row.compatibility = calculateCompatibility(row);
+                        console.log("Compatibility calculated for row:", row.compatibility);
+                    });
+                    // console.log("Result after adding compatibility:", result);
+                    resolve(result);
+                }
+            });
+        }
     });
 };
