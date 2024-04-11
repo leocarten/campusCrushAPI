1de75ad36e932d105aae761ec0be6e7e60654c1f
leocarten <leothelinuxguy@outlook.com>
2024-04-11 08:36:12 -0400

    Allow user to delete account v2

--- endpoints/deleteAccount.js
+++ endpoints/deleteAccount.js
@@ -14,7 +14,7 @@ export const deleteAcc = (token) => {
       const deleteFromMessagesInterface = 'delete from all_messages_interface where originalSenderID = ? or originalRecieverID = ?';
       // going to need a loop to delete all the messages for the user:
     //   for(var i = 0; i < getConvoIDs[0].convoID.length; )  
-      pool.query(getConvoIDs, [id], (getConvoIDError, convoIDs_) => {
+      pool.query(getConvoIDs, [id, id], (getConvoIDError, convoIDs_) => {
         if(getConvoIDError){
             reject(getConvoIDError)
         }else{
