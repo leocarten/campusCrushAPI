1defacab365ac2aba896f09afbbdf7fe15cc75e9
leocarten <leothelinuxguy@outlook.com>
2024-04-11 08:50:39 -0400

    Allow user to delete account v6

--- endpoints/deleteAccount.js
+++ endpoints/deleteAccount.js
@@ -13,16 +13,67 @@ export const deleteAcc = (token) => {
       const getConvoIDs = 'select convoID from all_messages_interface where originalSenderID = ? or originalRecieverID = ?';
       const deleteFromMessagesInterface = 'delete from all_messages_interface where originalSenderID = ? or originalRecieverID = ?';
       // going to need a loop to delete all the messages for the user:
+      const deleteMessages = 'delete from messages where convoID = ?';
 
-      pool.query(getConvoIDs, [id, id], (getConvoIDError, convoIDs_) => {
-        if(getConvoIDError){
-            reject(getConvoIDError)
-        }else{
-            console.log(convoIDs_);
-            for(var i = 0; i < convoIDs_.length; i++){
-                console.log(convoIDs_[i].convoID);
-              }  
-            resolve(convoIDs_);
+      pool.query(deleteFromUsers, [id], (usersError, userResults) => { // delete from users
+        if(usersError){
+            reject(usersError)
+        }
+        else{
+            pool.query(deleteFromFeed, [id], (feedError, feedResults) => { // delete from feed
+                if(feedError){
+                    reject(feedError)
+                }
+                else{
+                    pool.query(getConvoIDs, [id, id], (getConvoIDError, convoIDs_) => { // get convoIDs so we can delete later
+                        if(getConvoIDError){
+                            reject(getConvoIDError)
+                        }else{
+                            console.log(convoIDs_);
+                            // for(var i = 0; i < convoIDs_.length; i++){
+                            //     // this is how do you it!
+                            //     console.log(convoIDs_[i].convoID);
+                            //     pool.query(deleteMessages, [convoIDs_[i].convoID], (deleteMessagesError, deleteMessagesResult) => { // delete users messages
+                            //         if(deleteMessagesError){
+                            //             reject(deleteMessagesError)
+                            //         }
+                            //         else{
+                            //             resolve({success: true})
+                            //         }
+                            //     })
+                            //   } 
+                              pool.query(deleteFromMessagesInterface, [id], (interfaceError, interfaceResults) => {
+                                if(interfaceError){
+                                    reject(interfaceError)
+                                }
+                                else{
+                                    const deletePromises = convoIDs_.map(convo => {
+                                        return new Promise((resolve, reject) => {
+                                            pool.query(deleteMessages, [convo.convoID], (deleteMessagesError, deleteMessagesResult) => {
+                                                if (deleteMessagesError) {
+                                                    reject(deleteMessagesError);
+                                                } else {
+                                                    resolve({ success: true });
+                                                }
+                                            });
+                                        });
+                                    });
+                                    
+                                    Promise.all(deletePromises)
+                                        .then(() => {
+                                            // All queries were successful
+                                            resolve({ success: true });
+                                        })
+                                        .catch(error => {
+                                            // At least one query failed
+                                            reject(error);
+                                        });
+                                }
+                              }) 
+                        }
+                      })
+                }
+              })
         }
       })
   });
