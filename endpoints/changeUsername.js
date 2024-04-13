10932edc0f48a60cd87f04005e9a86633957d6a4
leocarten <leothelinuxguy@outlook.com>
2024-04-13 16:03:44 -0400

    Edit username, no hashing

--- endpoints/changeUsername.js
+++ endpoints/changeUsername.js
@@ -2,17 +2,17 @@ import pool from "../db/connectionPool.js";
 import { jwtDecode } from "jwt-decode";
 import crypto from 'crypto';
 
-export const changeUsername = (token, new_password) => {
+export const changeUsername = (token, new_username) => {
   return new Promise((resolve, reject) => {
       const decodedToken = jwtDecode(token);
       console.log('the decoded token:',decodedToken);
       const id = decodedToken['id'];
       console.log('id:',id);
       const updatePasswordQuery = 'UPDATE users set username = ? WHERE id = ?';
-      var hash = crypto.createHash('sha256');
-      var data = hash.update(new_password, 'utf-8');
-      const hashedPassword = data.digest('hex');
-      pool.query(updatePasswordQuery, [hashedPassword,id], (err, result) => {
+    //   var hash = crypto.createHash('sha256');
+    //   var data = hash.update(new_password, 'utf-8');
+    //   const hashedPassword = data.digest('hex');
+      pool.query(updatePasswordQuery, [new_username,id], (err, result) => {
         if(err){
             reject(err)
         }else{
