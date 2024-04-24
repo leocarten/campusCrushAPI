d31a2de7887cc1152f15112777d0d8c6f8c808cc
leocarten <leothelinuxguy@outlook.com>
2024-04-24 12:00:33 -0400

    Query for new tokens

--- endpoints/authenticateUser.js
+++ endpoints/authenticateUser.js
@@ -36,17 +36,26 @@ export const authenticateUser = (req, isAccessToken) => {
         else{
             if(authenticateUsersJWT(tokenToUse, process.env.REFRESH_SECRET_KEY) != false){
                 const userID = authenticateUsersJWT(tokenToUse, process.env.REFRESH_SECRET_KEY)['id'];
-                const userFilters = authenticateUsersJWT(tokenToUse, process.env.REFRESH_SECRET_KEY)['filter'];
-                const genderUserWantsToBeShown = authenticateUsersJWT(tokenToUse, process.env.REFRESH_SECRET_KEY)['genderUserWantsToSee'];
-                // const accessAge = getRandomNumber(50,80);
-                // const accessAgeToMinutes = accessAge * 60;
-                const accessAge = getRandomNumber(5,10);
-                const accessAgeToMinutes = accessAge * 1;
-                const refreshAge = getRandomNumber(7,11);
-                const refreshAgeToDays = refreshAge * 24 * 60 * 60;
-                const accessToken = generateAccessAndRefreshToken(userID, process.env.ACCESS_SECRET_KEY, 'access', accessAgeToMinutes, genderUserWantsToBeShown, 'filter...');
-                const refreshToken = generateAccessAndRefreshToken(userID, process.env.REFRESH_SECRET_KEY, 'refresh', refreshAgeToDays, genderUserWantsToBeShown, 'filter...');
-                resolve({success: true, message:"We had to re-assign the access and refresh token", access: accessToken, refresh: refreshToken});
+
+                // write a query to get wants_to_be_shown, lat, and long_
+                const query = 'SELECT genderUserWantsToSee, lat, long_ from info_to_display where id = ?';
+                pool.query(query, [userID], (querryError, results_) => {
+                    if(querryError){
+                        resolve(querryError)
+                    }else{
+                        const lat = results_[0].lat;
+                        const long_ = results_[0].long_;
+                        const wants_to_be_shown = results_[0].genderUserWantsToSee;
+                        const accessAge = getRandomNumber(50,80);
+                        const accessAgeToMinutes = accessAge * 60;
+                        const refreshAge = getRandomNumber(7,11);
+                        const refreshAgeToDays = refreshAge * 24 * 60 * 60;
+                        const proximity = 50;
+                        const accessToken = generateAccessAndRefreshToken(user_id, process.env.ACCESS_SECRET_KEY, 'access', accessAgeToMinutes, wants_to_be_shown, 'filter...', lat, long_, proximity);
+                        const refreshToken = generateAccessAndRefreshToken(user_id, process.env.REFRESH_SECRET_KEY, 'refresh', refreshAgeToDays, wants_to_be_shown, 'filter...', lat, long_, proximity);
+                        resolve({success: true, message:"We had to re-assign the access and refresh token", access: accessToken, refresh: refreshToken});
+                    }
+                })
             }else{
                 resolve({success: false, message: "We need the client to login again..."});
             }
